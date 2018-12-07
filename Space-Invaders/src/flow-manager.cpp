aa2b4ec0bf9b608fc996b1c1ff79e57e41ede649
pancreasio <julianholisu@gmail.com>
2018-12-07 06:51:50 -0300

    v1.1

--- Space-Invaders/src/flow-manager.cpp
+++ Space-Invaders/src/flow-manager.cpp
@@ -4,7 +4,7 @@ namespace game {
 	bool gameSwitch, mute;
 	const unsigned int screenheight = 720;
 	const unsigned int screenwidth = 1280;
-	const unsigned int version = 1, subVersion = 0;
+	const unsigned int version = 1, subVersion = 1;
 	Vector2 mousePosition;
 	void generalDraw() {
 		BeginDrawing();
