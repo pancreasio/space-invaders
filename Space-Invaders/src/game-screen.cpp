09a68d8f4cdc9c7179b6b3a5a549f1ec69d75d3e
pancreasio <julianholisu@gmail.com>
2018-12-06 22:53:55 -0300

    added background and fixed allignment

--- Space-Invaders/src/game-screen.cpp
+++ Space-Invaders/src/game-screen.cpp
@@ -7,9 +7,8 @@ namespace game {
 		Shot player1ShotArray[maxFriendlyShots], enemyArrayShotArray[maxEnemyShots];
 		Enemy enemyArray[maxEnemies];
 		Fortress fortressArray[maxFortresses];
-		Texture playerTexture, swarmTexture, bomberTexture, fortressTexture, shotTexture, explosionTexture;
+		Texture playerTexture, swarmTexture, bomberTexture, fortressTexture, shotTexture, explosionTexture, background;
 		unsigned int frameSpeed, currentFrame, frameCounter;
-		bool aux = false;
 
 		void returnToMenu() {
 			menuspace::initMenu();
@@ -23,19 +22,20 @@ namespace game {
 			fortressTexture = LoadTexture("res/assets/fortress.png");
 			shotTexture = LoadTexture("res/assets/shot.png");
 			explosionTexture = LoadTexture("res/assets/explosionsheet.png");
+			background = LoadTexture("res/assets/background.png");
 			for (int i = 0; i < maxEnemies; i++) {
 				enemyArray[i].type = swarm;
 				enemyArray[i].active = true;
 			}
 			//player settings
-			player1.position = {static_cast<float>(GetScreenWidth()),static_cast<float>(GetScreenHeight())};
+			player1.position = { static_cast<float>(GetScreenWidth()),static_cast<float>(GetScreenHeight())+260.0f };
 			player1.speed = 400.0f;
 
 			//player animation settings
-			player1.scale = 1.4;
-			player1.sourceRec = {0.0f, 0.0f, static_cast<float>(playerTexture.width) / 2.0f ,static_cast<float>(playerTexture.height)};
-			player1.destRec = {player1.position.x,player1.position.y, static_cast<float>(playerTexture.width) * player1.scale, static_cast<float>(playerTexture.height) * 2.0f * player1.scale};
-			frameSpeed = 1.0f;
+			player1.scale = 1.4f;
+			player1.sourceRec = { 0.0f, 0.0f, static_cast<float>(playerTexture.width) / 2.0f ,static_cast<float>(playerTexture.height) };
+			player1.destRec = { player1.position.x,player1.position.y, static_cast<float>(playerTexture.width) * player1.scale, static_cast<float>(playerTexture.height) * 2.0f * player1.scale };
+			frameSpeed = 1;
 			frameCounter = 0;
 			currentFrame = 0;
 
@@ -57,22 +57,21 @@ namespace game {
 			//ship animation 
 			frameCounter++;
 
-			if (frameCounter >= (2.0 / frameSpeed))
-			{
+			if (frameCounter >= (2.0 / frameSpeed)){
 				frameCounter = 0;
 				currentFrame++;
 
-				if (currentFrame > 1) currentFrame = 0;
-
-				player1.sourceRec.x = static_cast<float>(currentFrame)*static_cast<float>(playerTexture.width) / 2.0;
+				if (currentFrame > 1) {
+					currentFrame = 0;
+				}
+				player1.sourceRec.x = static_cast<float>(currentFrame)*static_cast<float>(playerTexture.width) / 2.0f;
 			}
 			player1.destRec = { player1.position.x,player1.position.y,static_cast<float>(playerTexture.width) * player1.scale,static_cast<float>(playerTexture.height) * 2 * player1.scale };
 		}
 
 		void drawGameplay() {
-			//DrawTexture(playerTexture,GetScreenWidth() /2, GetScreenHeight() /2, WHITE);
-
-			if(!aux) DrawTexturePro(playerTexture, player1.sourceRec, player1.destRec, {static_cast<float>(GetScreenWidth()) / 2.0f, static_cast<float>(GetScreenHeight()) / 2.0f}, 0.0f, WHITE);
+			DrawTextureEx(background, { 0.0f,0.0f }, 0.0f, 8.0f, WHITE);
+			DrawTexturePro(playerTexture, player1.sourceRec, player1.destRec, { static_cast<float>(GetScreenWidth()) / 2.0f, static_cast<float>(GetScreenHeight()) / 2.0f }, 0.0f, WHITE);
 		}
 	}
 }
